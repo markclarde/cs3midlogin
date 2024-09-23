ae50fcd40592f0306b43fa40a7330aea59337f1d
Mark <mcac022803@gmail.com>
2024-09-23 12:46:55 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Navigation Example',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+        '/pageOne': (context) => const PageOne(),
+        '/pageTwo': (context) => const PageTwo(),
+      },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/pageOne');
+              },
+              child: const Text('Go to Page One'),
+            ),
+            const SizedBox(width: 200, height: 500,),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/pageTwo');
+              },
+              child: const Text('Go to Page Two'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PageOne extends StatelessWidget {
+  const PageOne({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page One'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Back to Home'),
+        ),
+      ),
+    );
+  }
+}
+
+class PageTwo extends StatelessWidget {
+  const PageTwo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page Two'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Back to Home'),
+        ),
+      ),
+    );
+  }
+}
